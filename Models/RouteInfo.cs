dd6b2933f05e78cde2ff92387bf75562a237f056
Sergi Ortiz Gómez <drualcman@msn.com>
2023-04-21 15:22:39 +0800

    fixed internalinseconds variable to set like double

--- Models/RouteInfo.cs
+++ Models/RouteInfo.cs
@@ -6,11 +6,11 @@ public class RouteInfo
     public DateTime TravelStartTime { get; }
     public double SpeedKmHr { get; }
     public double RouteDistanceKm { get; }
-    public long NotificationIntervalInSeconds { get; }
+    public double NotificationIntervalInSeconds { get; }
     public Action<PositionNotification> CallBack { get;  }
 
     public RouteInfo(int routeId, PositionTrackerLatLong destination, DateTime travelStartTime, 
-        double speedKmHr, double routeDistanceKm, long notificationIntervalInSeconds, 
+        double speedKmHr, double routeDistanceKm, double notificationIntervalInSeconds, 
         Action<PositionNotification> callBack)
     {
         RouteId = routeId;
